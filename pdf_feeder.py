375fdd4b92b84679a7d8281d024c0d4af6ceec36
Keigh Rim <keigh.rim@gmail.com>
2020-03-26 16:05:32 -0400

    refactored file readers

---
+++ pdf_feeder.py
@@ -0,0 +1,59 @@
+import sys
+from urllib import request
+import os
+
+import feedparser
+
+
+class DataFeeder(object):
+    def feed(self):
+        raise NotImplementedError
+
+
+class LocalPDFFeeder(DataFeeder):
+    def __init__(self, path_to_pdf):
+        self.root = path_to_pdf
+
+    def feed(self):
+        for r, ds, fs in os.walk(self.root):
+            for f in fs:
+                if f.endswith(".pdf"):
+                    yield open(f, 'r')
+
+
+class ArxivFeeder(DataFeeder):
+
+    def __init__(self, keywords=None):
+        if keywords is None:
+            self.keywords = ['covid19', 'coronavirus', 'covid']
+        else:
+            self.keywords = keywords
+
+    def query(self):
+        feedparser._FeedParserMixin.namespaces['http://a9.com/-/spec/opensearch/1.1/'] = 'opensearch'
+        feedparser._FeedParserMixin.namespaces['http://arxiv.org/schemas/atom'] = 'arxiv'
+
+        qterm = '+OR+'.join(self.keywords)
+        start = 0
+        total = sys.maxsize  # will be updated once the first request gets response with total_matched_doc_num information
+        pagination = 2000
+
+        while start < total:
+            q = f'http://export.arxiv.org/api/query?search_query={qterm}&start={start}&max_results={pagination}'
+            response = request.urlopen(q).read()
+            feed = feedparser.parse(response)
+
+            # update looping variables
+            total = int(feed.feed.opensearch_totalresults)
+            start = start + pagination
+            for entry in feed.entries:
+                article_url = entry['link']
+                yield f"{article_url[:17]}pdf{article_url[20:]}.pdf"
+                # yield entry["link"]
+
+    def feed(self):
+        for url in self.query():
+            fname, _ = request.urlretrieve(url)
+            yield open(fname, 'r')
+
+
